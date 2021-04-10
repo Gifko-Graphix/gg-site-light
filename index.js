6257f643b5906e96d77e458d24298b33298e1cb4
EdwinAgbakpe <kagbakpe@gmail.com>
2021-04-10 16:08:37 +0200

    add view files + paths

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+
+const app =  express();
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.static("public"))
+
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.get("/template", function(req, res){
+    res.render("template");
+});
+
+app.get("/portfolio", function(req, res){
+    res.render("portfolio");
+});
+
+app.listen(9000, function(){
+    console.log("Listening on port 9000");
+});
