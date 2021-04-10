aabe7743918353393324b0088bf2041a3bbd36b2
EdwinAgbakpe <kagbakpe@gmail.com>
2021-04-10 17:15:56 +0200

    edit template route

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ app.get("/about", function(req, res){
     res.render("about");
 });
 
-app.get("/template", function(req, res){
-    res.render("template");
+app.get("/t", function(req, res){
+    // res.render("template");
+    res.render("template", {title: "req.params.title"});
 });
 
 app.get("/portfolio", function(req, res){
