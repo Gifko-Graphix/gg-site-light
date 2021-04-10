463d6fcd4222e3f2582a349f3be713639bca1730
Peter-Newman Messan <p.messan@jacobs-university.de>
2021-04-10 22:37:37 +0200

    finally figured out the image extern folder thing

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const fs = require("fs");
+
+mongoose.connect("mongodb+srv://admin-gifkographix:5ds5lazt2rg1b3TH@gg-cluster0.yuzpe.mongodb.net/websiteDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
+
+//create schemas
+
+const itemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    folder: {
+        type: String,
+        required: true
+    }
+})
+
+const PortfolioItem = mongoose.model("item", itemSchema)
 
 const app =  express();
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"))
+
+
+app.use(express.static(__dirname + "/public"));
+
+app.use('/portfolio-images', express.static( __dirname + '/../images/Portfolio'));
 
 app.set("view engine", "ejs")
 
@@ -18,7 +46,16 @@ app.get("/about", function(req, res){
 
 app.get("/t/:title", function(req, res){
     // res.render("template");
-    res.render("template", {title: req.params.title});
+    const req_title = req.params.title;
+
+    //find the doc using the title
+    PortfolioItem.find({title: req_title}, (err, doc) => {
+        const description = doc[0].description;
+        const folder = doc[0].folder;
+        const files = fs.readdirSync(__dirname + "/../images/Portfolio" + folder);
+        res.render("template", {title: req_title, description: description, folder: folder, files: files});
+    })
+    
 });
 
 app.get("/portfolio", function(req, res){
