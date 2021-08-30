f9de768118c0e7b22404684c2219c135ad1660ec
pmessan <p.newman.messan@gmail.com>
2021-08-30 20:08:30 +0200

    add eslint-config-next

--- components/ImagePortfolioItem.jsx
+++ components/ImagePortfolioItem.jsx
@@ -8,9 +8,9 @@ export default function ImagePortfolioItem({ item: { folder, title }, files }) {
     <div className="w-48 h-56 inline-block">
       <div className="relative h-48 w-48 object-cover object-center z-10 rounded-sm border-2 border-eggshell-default">
         {projects.map((project) => (
-          <Carousel interval="2500" key={uuidv4()} className="relative" style={{ height: '188.5', width: '188.5' }}>
+          <Carousel interval="2500" key={uuidv4()} className="relative">
             {project.filenames.map((file) => (
-              <Carousel.Item key={uuidv4()} className="object-cover object-center">
+              <Carousel.Item key={uuidv4()} className="w-full object-cover object-center overflow-hidden">
                 <Image
                   src={folder + file}
                   width="190"
