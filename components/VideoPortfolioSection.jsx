e7b83350de0db81f3543c66ae2ece603258a768d
TsayemDaniela <tsadaniela@gmail.com>
2021-09-10 09:52:05 +0200

    Build

--- components/VideoPortfolioSection.jsx
+++ components/VideoPortfolioSection.jsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { lowerCase } from 'lodash';
 import { v4 as uuidv4 } from 'uuid';
 import styled, { keyframes } from 'styled-components';
 import { fadeIn, fadeInUp } from 'react-animations';
@@ -29,7 +28,7 @@ export default function VideoPortfolioSection({
       <div className="mt-5 flex flex-col items-center ">
         <SectionHeader text={category} />
         <div className="flex flex-col gap-y-8 sm:gap-y-5 sm:grid md:grid-cols-3 md:gap-x-12 lg:grid-cols-4 lg:gap-x-11 xl:grid-cols-6 xl:gap-x-16 xl:gap-y-8 pt-4">
-          {(items.filter((item) => item.category === "videos")).map((item, index) => (
+          {(items.filter((item) => item.category === 'videos')).map((item, index) => (
             <FadeInDiv key={uuidv4()} delay={index} parentDelay={position}>
               <Link id={index} href={`/portfolio/${item.title}`} passHref>
                 <ItemButton>
