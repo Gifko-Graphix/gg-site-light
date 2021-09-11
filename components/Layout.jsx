5e6133d75b823481c4af334f82026cafe1ed76df
pmessan <p.newman.messan@gmail.com>
2021-09-11 09:26:43 +0200

    fix div page spanning issue

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -13,7 +13,7 @@ const FadeInDiv = styled.div`
 
 export default function Layout({ children, currentPage, currentPageDescription }) {
   return (
-    <>
+    <html lang="en">
       <Head>
         <meta charSet="utf-8" />
         <meta
@@ -39,13 +39,13 @@ export default function Layout({ children, currentPage, currentPageDescription }
         <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
 
-      <div className="absolute overflow-auto bg-textured text-eggshell-default inset-0" id="background">
+      <body className="absolute overflow-auto bg-textured text-eggshell-default inset-0 min-h-screen" id="background">
         <Header />
         <div className="mx-8 md:mx-12 lg:mx-16 xl:mx-24 mt-24">
           {children}
         </div>
 
-        <footer className="relative bottom-0 w-full flex flex-row items-center justify-center text-gray-300 py-10 px-8 text-center text-xs">
+        <footer className="relative self-end bottom-0 w-full flex flex-row items-center justify-center text-gray-300 py-10 px-8 text-center text-xs">
           <FadeInDiv>
             Copyright &copy;
             {' '}
@@ -55,10 +55,10 @@ export default function Layout({ children, currentPage, currentPageDescription }
           </FadeInDiv>
         </footer>
 
-      </div>
+      </body>
 
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" />
 
-    </>
+    </html>
   );
 }
