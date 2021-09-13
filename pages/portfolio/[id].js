0b2a80d7cdf0d17eb04986e5ffdd79e3560fd4de
pmessan <p.newman.messan@gmail.com>
2021-09-13 06:37:30 +0200

    cloudinary integration pt2 - dynamic page

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,13 +1,16 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable no-underscore-dangle */
-import Image from 'next/image';
-import fs from 'fs';
-import ReactPlayer from 'react-player';
+// import Image from 'next/image';
+// import fs from 'fs';
+// import ReactPlayer from 'react-player';
 import { v4 as uuidv4 } from 'uuid';
 import { Worker, Viewer, SpecialZoomLevel } from '@react-pdf-viewer/core';
-import path from 'path';
+// import path from 'path';
 import styled, { keyframes } from 'styled-components';
 import { fadeInDown, fadeIn, fadeInUp } from 'react-animations';
+import cloudinary from 'cloudinary';
+import { AdvancedImage, AdvancedVideo } from '@cloudinary/react';
+import { Cloudinary } from '@cloudinary/url-gen';
 import Layout from '../../components/Layout';
 import dbConnect from '../../utils/dbConnect';
 import Item from '../../models/Item';
@@ -38,6 +41,12 @@ export default function Project({
     folder, title, description, customer, category, profileLink,
   }, files,
 }) {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'gifkographix',
+    },
+  });
+
   return (
     <Layout currentPage={title} currentPageDescription={`Come find out more about our ${title} project right here!`}>
       <div className="flex flex-col-reverse sm:grid sm:grid-cols-2 sm:gap-x-10 w-full h-full">
@@ -52,9 +61,9 @@ export default function Project({
           && (
             <FadeInUpDiv>
               <div className="grid md:gap-x-5 gap-y-2 sm:overflow-auto sm:grid-cols-1 xl:grid-cols-2 mt-0 sm:mt-4 " style={{ height: '550px' }}>
-                {files.filter((filename) => path.extname(filename) === '.webp').map((file) => (
+                {files.map((file) => (
                   <div key={uuidv4()} className="relative object-center col-span-1 w-full" style={{ height: '370px' }}>
-                    <Image src={`/static${folder}${file}`} alt={file} layout="fill" objectFit="contain" />
+                    <AdvancedImage className="" cldImg={cld.image(file.public_id)} />
                   </div>
                 ))}
               </div>
@@ -65,9 +74,9 @@ export default function Project({
           && (
             <FadeInUpDiv>
               <div className="grid md:gap-x-5 gap-y-4 sm:grid-cols-1 mt-0 sm:mt-4 sm:overflow-auto" style={{ height: '550px' }}>
-                {files.filter((filename) => path.extname(filename) === '.mp4').map((file) => (
-                  <div key={uuidv4()} className="relative col-span-full">
-                    <ReactPlayer width="" height="" url={`/static${folder}${file}`} pip stopOnUnmount={false} controls />
+                {files.map((file) => (
+                  <div key={uuidv4()} className="relative w-full">
+                    <AdvancedVideo cldVid={cld.video(file.public_id)} controls className="flex-grow h-48 object-cover shadow-xl" />
                   </div>
                 ))}
               </div>
@@ -78,7 +87,7 @@ export default function Project({
           && (
             <FadeInUpDiv>
               <div className="grid md:gap-x-5 gap-y-4 sm:grid-cols-1 xl:grid-cols-2 mt-0 sm:mt-4 overflow-auto">
-                {files.filter((filename) => path.extname(filename) === '.pdf').map((file) => (
+                {files.filter((filename) => filename.format === '.pdf').map((file) => (
                   <div key={uuidv4()} className="relative object-cover overflow-hidden object-center col-span-full inline-block">
                     <Worker key={uuidv4()} workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
                       <div key={uuidv4()} style={{ height: '580px' }}>
@@ -123,17 +132,17 @@ export default function Project({
 
 export async function getStaticPaths() {
   await dbConnect();
-  const projectFiles = []; // array of objects with project title & file names
+  // const projectFiles = []; // array of objects with project title & file names
 
   /* find all the data in our database */
   const result = await Item.find({});
   const items = result.map((doc) => {
     const item = doc.toObject();
     item._id = item._id.toString();
-    projectFiles.push({
-      title: item.title,
-      filenames: fs.readdirSync(`${process.cwd()}/public/static${item.folder}`),
-    });
+    // projectFiles.push({
+    //   title: item.title,
+    //   filenames: fs.readdirSync(`${process.cwd()}/public/static${item.folder}`),
+    // });
     return item;
   });
 
@@ -158,7 +167,17 @@ export async function getStaticProps({ params }) {
   const item = result[0].toObject();
   item._id = item._id.toString();
 
-  const files = fs.readdirSync(`${process.cwd()}/public/static${item.folder}`);
+  // const files = fs.readdirSync(`${process.cwd()}/public/static${item.folder}`);
+
+  const response = await cloudinary.v2.search.expression(
+    `folder:websiteFiles${item.folder}*`, // add your folder
+  ).sort_by('public_id', 'desc').execute();
+
+  const files = response.resources.map((file) => (
+    {
+      public_id: file.public_id,
+      format: file.format,
+    }));
 
   return { props: { item, files } };
 }
