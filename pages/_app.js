4b74fb517e68057b08d11c9858047b5f8ca67877
EdwinAgbakpe <kagbakpe@gmail.com>
2021-09-23 17:18:06 +0000

    chore(tests): Set up testing framework - Add setup files for Cypress - Add sample spec
    Unrelated
    - fix a linting bug in Layout.jsx

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,29 @@
+import Script from 'next/script';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_MEASUREMENT_ID}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.GOOGLE_ANALYTICS_MEASUREMENT_ID});
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
