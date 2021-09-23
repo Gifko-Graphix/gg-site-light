1355a6b824c15dd827c1bb4536add437f06a0d0d
EdwinAgbakpe <kagbakpe@gmail.com>
2021-09-23 21:52:28 +0000

    add ID to code

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  const { NEXT_PUBLIC_GOOGLE_ANALYTICS } = process.env;
   return (
     <>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        src="https://www.googletagmanager.com/gtag/js?id=G-22K64RX0GK"
       />
 
       <Script strategy="lazyOnload">
@@ -18,7 +17,7 @@ function MyApp({ Component, pageProps }) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', ${NEXT_PUBLIC_GOOGLE_ANALYTICS});
+          gtag('config', 'G-22K64RX0GK');
         `}
       </Script>
 
